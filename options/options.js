348f2ef5247064adaa8d8b0b254e66a5835ad829
Crowdbotics <team@crowdbotics.com>
2023-11-28 22:57:35 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "mixpanel-demo-44789",
+  url: "https://mixpanel-demo-44789.botics.co",
+  api: "https://mixpanel-demo-44789.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
